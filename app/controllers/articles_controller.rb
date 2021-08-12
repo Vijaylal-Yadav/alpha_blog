5895e4065574447cb4714b799acf0412750da746
Vijaylal_Yadav <vijayyadav.vy5577@gmail.com>
2021-08-12 16:45:02 +0530

    Adding controller for new and create actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,19 @@
 class ArticlesController < ApplicationController
-	def show
-		@article = Article.find(params[:id])
-	end
+  def show
+    @article = Article.find(params[:id])
+  end
 
-	def index
-		@article = Article.all
-	end
+  def index
+    @article = Article.all
+  end
+
+  def new
+  end
+
+  def create
+  	@article = Article.new(params.require(:article).permit(:title, :description))
+  	@article.save
+  	#render plain: @article.inspect
+  	redirect_to @article
+  end
 end
