637fc59c13e05b7d8e34d5a2888ca6740225e424
Vijaylal_Yadav <vijayyadav.vy5577@gmail.com>
2021-08-12 20:03:13 +0530

    Added layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  root "pages#home"
+  resources :articles    #This will provide all the RestFul routes for the articles
   get '/home', to: "pages#home"
   get '/about', to: "pages#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
